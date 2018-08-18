8cef404b54c82348a281f21bda27f412e60c0b4d
Ian Leonard <antonlacon@gmail.com>
2018-08-18 06:03:55 +0000

    test-mksquashfs: compresion levels, zstd support
    Add support to benchmark compression levels with mksquashfs.
    
    Remove xz zupport, and add zstd support.
    
    Pre-populate the "le-filesystem" directory to be used in benching.
    
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- report.sh
+++ report.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+# delete "|sort -V" if using BusyBox sort
+
 echo "Filename, Size, Ratio, CTIME, CRATE, UTIME, URATE"
-cd results
-for FILENAME in $(ls *squashfs | sort -V); do
+for FILENAME in $(ls ./results/*squashfs | sort -V); do
     FILESIZE=$(du $FILENAME | awk '{print $1}')
     RATIO=$(grep "of uncompressed filesystem" $FILENAME.results | awk '{print $1}')
     CTIME=$(cat $FILENAME.compress_time | grep real | awk '{print $2}')
--- test-mksquashfs.sh
+++ test-mksquashfs.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
+# run as root
+
 # Set up environment
 echo " * Setting up..."
-mkdir -p squashfs
 mkdir -p tmpfs
 mkdir -p results
-mount filesystem.squashfs squashfs
-mount -t tmpfs none tmpfs -o size=80%
-cp -R squashfs/. tmpfs/
+mount -t tmpfs none tmpfs
+cp -R le-filesystem/. tmpfs/
 
 # Crunch
-for COMPRESSION_TYPE in gzip lzo xz; do
+for COMPRESSION_TYPE in gzip lzo zstd; do
     echo " - Testing $COMPRESSION_TYPE"
-    for BLOCK_SIZE in 4096 8192 16384 32768 65536 131072; do
-    FILENAME="results/squashfs-$COMPRESSION_TYPE-$BLOCK_SIZE.squashfs"
-    echo "   * Running a squashfs using compression $COMPRESSION_TYPE, blocksize $BLOCK_SIZE"
-    ( time mksquashfs tmpfs $FILENAME \
-                      -b $BLOCK_SIZE -comp $COMPRESSION_TYPE -noappend ) \
-                      >  $FILENAME.results \
-                      2> $FILENAME.compress_time
+    if [ "$COMPRESSION_TYPE" = "lzo" -o "$COMPRESSION_TYPE" = "gzip" ]; then
+        COMPRESSION_LEVEL=(1 2 3 4 5 6 7 8 9)
+    elif [ "$COMPRESSION_TYPE" = "zstd" ]; then
+        COMPRESSION_LEVEL=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22)
+    fi
+
+    for COMPRESSION_STRENGTH in ${COMPRESSION_LEVEL[*]}; do
+        for BLOCK_SIZE in 4096 8192 16384 32768 65536 131072; do
+            FILENAME="results/squashfs-$COMPRESSION_TYPE-$COMPRESSION_STRENGTH-$BLOCK_SIZE.squashfs"
+            echo "   * Running a squashfs using compression $COMPRESSION_TYPE level $COMPRESSION_STRENGTH, blocksize $BLOCK_SIZE"
+            ( time mksquashfs tmpfs $FILENAME \
+                -b $BLOCK_SIZE -comp $COMPRESSION_TYPE -Xcompression-level $COMPRESSION_STRENGTH -noappend ) \
+                >  $FILENAME.results \
+                2> $FILENAME.compress_time
+        done
     done
 done
 
@@ -27,7 +35,7 @@ for FILENAME in $(ls results/*.squashfs); do
     mkdir -p squashfs.extract
     echo "   * Reading $FILENAME..."
     # Catting things in /dev... not so great idea (and it's trivially small anyway)
-    mount $FILENAME squashfs.extract
+    mount -t squashfs $FILENAME squashfs.extract
     (time $(find squashfs.extract | grep -v "/dev" | xargs cat - > /dev/null)) 2> $FILENAME.uncompress_time
     REALTIME=$(grep "real" $FILENAME.uncompress_time | \
                tail -n 1 | awk '{print $2}')
@@ -39,7 +47,5 @@ done
 
 # Clean up
 echo " * Cleaning up..."
-umount -l squashfs
 umount -l tmpfs
-rmdir squashfs
 rmdir tmpfs
